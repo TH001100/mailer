b0cf77d4e77498644cf97218dde6c0cd904aead2
blok-hamster <josicoded@gmail.com>
2024-08-01 15:43:32 +0100

    commit init

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  async sendMail(@Body() {domain, sendersName, sendersEmail, reciversName, receiversEmail, subject, emailBody, reply_to}:{domain:string, sendersName:string,sendersEmail:string, reciversName:string, receiversEmail:string, emailBody:string, subject:string, reply_to: string, html?:string}): Promise<any> {
+    try{
+      const done = await this.appService.sendMail({domain, sendersName, sendersEmail, reciversName, receiversEmail, subject, emailBody, reply_to});
+      return done;
+    }catch(e){
+      console.log(e.message)
+    }
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Resend } from 'resend';
+import * as dotenv from 'dotenv'
+dotenv.config()
+const resend = new Resend(process.env.RESEND_KEY);
+
+
+@Injectable()
+export class AppService {
+    async sendMail({domain, sendersName, sendersEmail, reciversName, receiversEmail, subject, emailBody, reply_to}:{domain:string, sendersName:string,sendersEmail:string, reciversName:string, receiversEmail:string, emailBody:string, subject:string, reply_to: string, html?:string}){
+      try{
+        const mailOptions = {
+          from: `${sendersName} <${sendersEmail}@${domain}>`,
+          to: receiversEmail,
+          subject: subject,
+          text: emailBody,
+          reply_to: reply_to === undefined ? "tryhard11220@gmail.com" : reply_to
+        }
+        console.log(mailOptions)
+        const sent = await resend.emails.send(mailOptions)
+      if(sent.error !== null) return {status: false, message: 'error sending message', data: null}
+      return {status: true, message: 'email sent succesfully', data: sent}
+    }catch(e){
+      console.log(e.message)
+      return {status: false, message: 'error sending message', data: null}
+    }
+  }
+}
